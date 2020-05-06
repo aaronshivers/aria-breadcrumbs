ce2ad708f37502a31189d9bcbc1532bb9541bc1c
aaronshivers <aaron@moonmail.org>
2020-05-06 12:51:32 -0500

    Updates

---
+++ precache-manifest.682fb5628c82a8ace85b4985ffe91cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56d51163543765bcb55928f51ffff90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "787cb7323f73fd56e18d",
+    "url": "/static/css/main.aa4c5d95.chunk.css"
+  },
+  {
+    "revision": "0f75229229c2fa74e14b",
+    "url": "/static/js/2.d7e9e1e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d7e9e1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787cb7323f73fd56e18d",
+    "url": "/static/js/main.c428da54.chunk.js"
+  },
+  {
+    "revision": "40ab2249e02e46925d97",
+    "url": "/static/js/runtime-main.f876e3ac.js"
+  }
+]);
